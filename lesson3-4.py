59f6a6b7ccc1d5db0b95fc4a3b28b31bc508fbaa
AnToNy_PrImE <83171264+Anton-Korol-dev@users.noreply.github.com>
2026-02-22 14:47:53 +0200

    example v.1

---
+++ lesson3-4.py
@@ -0,0 +1,26 @@
+# def first_function():
+#     pass
+
+# print(first_function)
+# print(first_function())
+# ctrl /
+# def second_function():
+#     hello = "Hello, World!"
+#     return hello
+# print(second_function())
+
+# def third_function(arg1, arg2):
+#     result = arg1 + arg2
+#     return result
+# print(third_function(5, 10))
+# x = int(input("Enter a first number: "))
+# y = int(input("Enter a second number: "))
+# print(third_function(x, y))
+
+def fourth_function(arg1, arg2):
+    return arg1 * arg2
+print(fourth_function(int(input("Enter a first number: ")),
+                      int(input("Enter a second number: "))))
+
+fourth_function(int(input("Enter a first number: ")),
+                      int(input("Enter a second number: ")))
